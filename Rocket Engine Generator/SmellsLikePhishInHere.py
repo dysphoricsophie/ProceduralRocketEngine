96b5cccb7b8477c40a0a2a47ac0c4d4d8f06ce2d
DysphoricSophie <sfsgamer4@gmail.com>
2024-03-24 18:33:44 +0400

    24/03/2024

--- Rocket Engine Generator/SmellsLikePhishInHere.py
+++ Rocket Engine Generator/SmellsLikePhishInHere.py
@@ -1,17 +1,6 @@
-totality = [["apple", "banana", "cherry"], ["kiwi", "mango", "grapefruit"]]
-productsData = totality[0]  # Assuming productsData is the first row of Totality
+def close(list, num):
+	a = min([i for i in list if num < i])
+	b = max([i for i in list if num > i])
+	return a, b
 
-ProA = "kiwi"
-ProB = "banana"
-
-indexA = -1
-indexB = -1
-
-for i in range(len(totality[0])):
-    if totality[0][i] == ProA:
-        indexA = i
-    if totality[0][i] == ProB:
-        indexB = i
-
-print(f"ProA is found at index {indexA} in the first row")
-print(f"ProB is found at index {indexB} in the first row")
+print(close([3, 8, 5, 9, 10], 6))
