eb956b87753cc12dd57530bbb4368179f01a4f90
Sophia the Hydrazine Addict <sfsgamer4@gmail.com>
2024-08-16 21:29:10 +0400

    Better Changes to Adiabatic Combustion Calculator

--- Rocket Engine Generator/AdiabaticCombustor3.py
+++ Rocket Engine Generator/AdiabaticCombustor3.py
@@ -111,10 +111,11 @@ def exponentF(oxid, fuel):
     return reaction #[reaction, delta_E[fuel_ListSample.index(fuel)]]
 def calculateHr(prodA, prodB, prodC, prods, coefs, A):
     hp = []
-    A1 = ["H2", "O2", "H2O", "CO2", "F2", "F2O2", "N2O4", "H2O2", "O3", "HNO3", "CH4", "C2H5OH", "C6H5NH2", "NH3",
-          "C2H8N2", "CH6N2", "N2H4", "CH3OH", "C12H26", "NO2", "CF4", "N2", "HF", "C6H7N"]
-    A2 = [0, 0, -228.582, 0, 0, 31.61, 9.079, -136.106, 142.674, -134.306, -74.873, -277.5, 0, -45.898, 48.9, 94.6,
-        95.353, -277.5, -350.9, 33.095, -393.522, -933.199, 0, -272.546, 86.81]
+    A1 = ['H2', 'O2', 'H2O', 'CO2', 'F2', 'F2O2', 'N2O4', 'H2O2', 'O3', 'HNO3', 'CH4', 'Ethanol', 'Aniline', 'Ammonia',
+          '1,1-dimethylhydrazine', 'Methylhydrazine', 'Hydrazine', 'Methanol', 'C12H26', 'NO2', 'CF4', 'N2', 'HF',
+          'Aniline', 'NF3']
+    A2 = [0, 0, -241.826, 0, 0, 31.61, 10.9, -135.403, 141.744, -134.19, -74.518, -235.03, 86.81, -45.556, 48.3, 54.14,
+          97.56, -200.92, -353.5, 34.071, -933.76, 0, -272.726, 86.81, -123]
 
     if A == 1:
         for t in range(0, len(prodA)):
@@ -234,8 +235,9 @@ def Enthaly_Calculator(t, species):
         HH_Tr = h_T - h_298
 
     # Checks the thermochemical properties using the thermo module
-    elif species == 'C2H5OH' or species == 'NH3' or species == 'NH4' or species == 'CH3OH' or species == "C6H5NH2" or species == "C2H8N2" \
-            or species == "CH6N2" or species == "N2H4" or species == "HF" or species == "C6H7N":
+    elif species == 'Ethanol' or species == 'Ammonia' or species == "Nitrogen Trifluoride" or species == 'Methanol'\
+        or species == "Aniline" or species == "1,1-dimethylhydrazine" or species == "Methylhydrazine" or species == "Hydrazine"\
+        or species == "HF" or species == "NF3":
         constants, correlations = ChemicalConstantsPackage.from_IDs([species])
         eos_kwargs = dict(Tcs=constants.Tcs, Pcs=constants.Pcs, omegas=constants.omegas)
 
@@ -243,8 +245,8 @@ def Enthaly_Calculator(t, species):
         gas = CEOSGas(PRMIX, HeatCapacityGases=correlations.HeatCapacityGases, eos_kwargs=eos_kwargs)
 
         flasher = FlashPureVLS(constants, correlations, gas=gas, liquids=[liquid], solids=[])
-        h_298 = flasher.flash(T=STP, P=ct.one_atm).H() / 1000
-        h_T = flasher.flash(T=t, P=ct.one_atm).H() / 1000
+        h_298 = flasher.flash(T=STP, P=ct.one_atm).H()/1000
+        h_T = flasher.flash(T=t, P=ct.one_atm).H()/1000
         HH_Tr = (h_T - h_298)
     return HH_Tr
 def close(arr, target):
@@ -272,6 +274,8 @@ def close(arr, target):
             larger = num
             break  # Exit loop once the first larger number is found
 
+    if smaller is None or larger is None:
+        raise IndexError(f"Index could not be dound within list, could not find {target} within {arr}")
     return [smaller, larger]
 def calculate(oxidizer, fuel):
     To = 298.15
@@ -280,25 +284,23 @@ def calculate(oxidizer, fuel):
     processed = reaction_processing(reaction)
     enth_reacA = processed[0]
     enth_reacB = processed[1]
-    enth_prodA = processed[2]
-    enth_prodB = processed[3]
-    enth_prodC = processed[4]
+    enth_prodA = list[float](processed[2])
+    enth_prodB = list[float](processed[3])
+    enth_prodC = list[float](processed[4])
     prods = processed[-3]
     coefs = processed[-2]
     A = processed[-1]
 
-    print(processed)
-
     Hp = calculateHr(enth_prodA, enth_prodB, enth_prodC, prods, coefs, A)
     Hr = ((float(coefs[0]) * float(enth_reacA)) + (float(coefs[1]) * float(enth_reacB)))
-    print(Hp, Hr)
 
-    minA = close(Hp, Hr)[0]
-    maxA = close(Hp, Hr)[1]
+    min_max = close(Hp, Hr)
+    minA = min_max[0]
+    maxA = min_max[1]
     minB = Hp.index(minA)
     maxB = Hp.index(maxA)
 
-    Temps = [100, 200, 298.15, 300, 400, 500, 600, 700, 800, 900, 1000, 1100, 1200, 1300, 1400, 1500, 1600, 1700, 1800,
+    Temps = [0, 100, 200, 298.15, 300, 400, 500, 600, 700, 800, 900, 1000, 1100, 1200, 1300, 1400, 1500, 1600, 1700, 1800,
              1900, 2000, 2100, 2200, 2300, 2400, 2500, 2600, 2700, 2800, 2900, 3000, 3100, 3200, 3300, 3400, 3500, 3600,
              3700, 3800, 3900, 4000, 4100, 4200, 4300, 4400, 4500, 4600, 4700, 4800, 4900, 5000, 5100, 5200, 5300, 5400,
              5500, 5600, 5700, 5800, 5900, 6000]
--- Rocket Engine Generator/qum.py
+++ Rocket Engine Generator/qum.py
@@ -3,14 +3,8 @@ from thermochem.janaf import Janafdb
 import cantera as ct
 
 def Enthaly_Calculator(t, species):
-    A1 = ["H2", "O2", "H2O", "CO2", "F2", "F2O2", "N2O4", "H2O2", "O3", "HNO3", "CH4", "C2H5OH", "C6H5NH2", "NH3",
-        "C2H8N2", "CH6N2", "N2H4", "CH3OH", "C12H26", "NO2", "CF4", "N2", "HF", "C6H7N"]
-    A2 = [0, 0, -228.582, 0, 0, 31.61, 9.079, -136.106, 142.674, -134.306, -74.873, -277.5, 0, -45.898, 48.9, 94.6,
-        95.353, -277.5, -350.9, 33.095, -393.522, -933.199, 0, -272.546, 54.54]
-
     HH_Tr = 0
     STP = 298.15
-    Hf = A2[A1.index(species)]
 
     # Checks the thermochemical properties using the thermochem module
     if species == 'H2' or species == 'O2' or species == 'H2O' or species ==  'CO2' or species ==  'F2' or species ==  'N2O4'\
@@ -36,8 +30,9 @@ def Enthaly_Calculator(t, species):
         HH_Tr = h_T - h_298
 
     # Checks the thermochemical properties using the thermo module
-    elif species == 'C2H5OH' or species == 'NH3' or species == 'NH4' or species == 'CH3OH' or species == "C6H5NH2" or species == "C2H8N2"\
-        or species == "CH6N2" or species == "N2H4" or species == "HF" or species == "C6H7N":
+    elif species == 'Ethanol' or species == 'Ammonia' or species == "Nitrogen Trifluoride" or species == 'Methanol'\
+        or species == "Aniline" or species == "1,1-dimethylhydrazine" or species == "Methylhydrazine" or species == "Hydrazine"\
+        or species == "HF" or species == "NF3":
         constants, correlations = ChemicalConstantsPackage.from_IDs([species])
         eos_kwargs = dict(Tcs=constants.Tcs, Pcs=constants.Pcs, omegas=constants.omegas)
 
@@ -48,18 +43,17 @@ def Enthaly_Calculator(t, species):
         h_298 = flasher.flash(T=STP, P=ct.one_atm).H()/1000
         h_T = flasher.flash(T=t, P=ct.one_atm).H()/1000
         HH_Tr = (h_T - h_298)
-    return HH_Tr, Hf
+    return HH_Tr
 
-list1 = ["H2", "O2", "H2O", "CO2", "F2", "F2O2", "N2O4", "H2O2", "O3", "HNO3", "CH4", "C2H5OH", "C6H5NH2", "NH3", "C2H8N2",
-         "CH6N2", "N2H4", "CH3OH", "C12H26", "NO2", "CF4", "N2", "HF", "C6H7N"]
-list2 = [0, 0, -228.582, 0, 0, 31.61, 9.079, -136.106, 142.674, -134.306, -74.873, -277.5, 0, -45.898, 48.9, 94.6, 95.353,
-         -277.5, -350.9, 33.095, -393.522, -933.199, 0, -272.546]
+A1 = ['H2', 'O2', 'H2O', 'CO2', 'F2', 'F2O2', 'N2O4', 'H2O2', 'O3', 'HNO3', 'CH4', 'Ethanol', 'Aniline', 'Ammonia',
+      '1,1-dimethylhydrazine', 'Methylhydrazine', 'Hydrazine', 'Methanol', 'C12H26', 'NO2', 'CF4', 'N2', 'HF',
+      'Aniline', 'NF3']
 
 Temps = [100, 200, 298.15, 300, 400, 500, 600, 700, 800, 900, 1000, 1100, 1200, 1300, 1400, 1500, 1600, 1700, 1800, 1900, 2000, 2100,
          2200, 2300, 2400, 2500, 2600, 2700, 2800, 2900, 3000, 3100, 3200, 3300, 3400, 3500, 3600, 3700, 3800, 3900, 4000, 4100, 4200,
          4300, 4400, 4500, 4600, 4700, 4800, 4900, 5000, 5100, 5200, 5300, 5400, 5500, 5600, 5700, 5800, 5900, 6000]
 for t_new in enumerate(Temps):
     print(f"\nAt temperature {t_new[1]}K: ")
-    for specie in enumerate(list1):
-        val = Enthaly_Calculator(t_new[1], specie[1])[0]
+    for specie in enumerate(A1):
+        val = Enthaly_Calculator(t_new[1], specie[1])
         print(f'{specie[0]+1} - Enthalpy difference or H-H(Tr) for {specie[1]} at {t_new[1]}K: {val:.2f} kJ/mol')
